bc185236de5e650f51982e9d4141e215cd50761e
Auxiguilar <filip.myras@gmail.com>
2025-11-12 12:33:59 +0100

    I am in the blissless realm beyond all understanding and mired in the depths of confusion...

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import functions, htmlnode, textnode
+import os, shutil
 
 
 def main():
-    text = ("## header2\n\na paragraph of text; **look!**\nand _here!_\n\n```\n# and my code is **broken**!\nprint('help'!)\n```\n\n1. my imports are fucked for some reason\n2. I don't know _why_!\n\n- another\n- list")
-    node = functions.markdown_to_html_node(text)
-    print(node.to_html())
+    pass
 
 
 if __name__ == '__main__':
